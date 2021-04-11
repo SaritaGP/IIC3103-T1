b23da81f76bf4c67b1ca29157c7fdbc180d9bc01
SaritaGP <saritabelen.gp@gmail.com>
2021-04-11 12:49:11 -0400

    funcional

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,4 +4,20 @@ class CharactersController < ApplicationController
     @character_api = HTTParty.get("https://tarea-1-breaking-bad.herokuapp.com/api/characters?name=#{name_character}")[0]
     @quotes_api = HTTParty.get("https://tarea-1-breaking-bad.herokuapp.com/api/quote?author=#{name_character}")
   end
+
+  def index  
+    search_name = params[:search]
+    @result_search = []
+    offset = 0
+
+    while @result_search.length % 10 == 0 || @result_search.empty?
+      result = HTTParty.get("https://tarea-1-breaking-bad.herokuapp.com/api/characters?name=#{search_name}&limit=10&offset=#{offset}")
+      @result_search.append(*result)
+      offset += 10
+      if result.empty?
+        break
+      end
+    end
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'characters/:id', as: 'character', to: 'characters#show'
+  get 'characters', as: 'characters', to: 'characters#index' 
   get 'episodes_bcs/:id', as: 'episode_bcs', to: 'episodes_bcs#show'
   get 'episodes_bb/:id', as: 'episode_bb', to: 'episodes_bb#show'
   get 'seasons_bcs/:id', as: "season_bcs", to: 'seasons_bcs#show'
